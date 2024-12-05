86ada706dac5eaf69e1bbca517c2c27f86f7ccef
sinasalek <salek.sina@gmail.com>
2024-12-05 06:37:19 +0000

    black

--- experiments/gnn/protein.py
+++ experiments/gnn/protein.py
@@ -44,78 +44,81 @@ from torch_geometric.nn import global_add_pool
 
 
 class GIN(torch.nn.Module):
-   def __init__(self, dim_h):
-      super(GIN, self).__init__()
-      self.conv1 = GINConv(
-         Sequential(
-               Linear(dataset.num_node_features, dim_h),
-               BatchNorm1d(dim_h),
-               ReLU(),
-               Linear(dim_h, dim_h),
-               ReLU(),
-         )
-      )
-      self.conv2 = GINConv(
-         Sequential(
-               Linear(dim_h, dim_h),
-               BatchNorm1d(dim_h),
-               ReLU(),
-               Linear(dim_h, dim_h),
-               ReLU(),
-         )
-      )
-      self.conv3 = GINConv(
-         Sequential(
-               Linear(dim_h, dim_h),
-               BatchNorm1d(dim_h),
-               ReLU(),
-               Linear(dim_h, dim_h),
-               ReLU(),
-         )
-      )
-      self.lin1 = Linear(dim_h*3, dim_h*3)
-      self.lin2 = Linear(dim_h*3, dataset.num_classes)
+    def __init__(self, dim_h):
+        super(GIN, self).__init__()
+        self.conv1 = GINConv(
+            Sequential(
+                Linear(dataset.num_node_features, dim_h),
+                BatchNorm1d(dim_h),
+                ReLU(),
+                Linear(dim_h, dim_h),
+                ReLU(),
+            )
+        )
+        self.conv2 = GINConv(
+            Sequential(
+                Linear(dim_h, dim_h),
+                BatchNorm1d(dim_h),
+                ReLU(),
+                Linear(dim_h, dim_h),
+                ReLU(),
+            )
+        )
+        self.conv3 = GINConv(
+            Sequential(
+                Linear(dim_h, dim_h),
+                BatchNorm1d(dim_h),
+                ReLU(),
+                Linear(dim_h, dim_h),
+                ReLU(),
+            )
+        )
+        self.lin1 = Linear(dim_h * 3, dim_h * 3)
+        self.lin2 = Linear(dim_h * 3, dataset.num_classes)
 
-   def forward(self, x, edge_index, batch):
-      # Node embeddings
-      h1 = self.conv1(x, edge_index)
-      h2 = self.conv2(h1, edge_index)
-      h3 = self.conv3(h2, edge_index)
-      # Graph-level readout
-      h1 = global_add_pool(h1, batch)
-      h2 = global_add_pool(h2, batch)
-      h3 = global_add_pool(h3, batch)
-      # Concatenate graph embeddings
-      h = torch.cat((h1, h2, h3), dim=1)
-      # Classifier
-      h = self.lin1(h)
-      h = h.relu()
-      h = F.dropout(h, p=0.5, training=self.training)
-      h = self.lin2(h)
-      return F.log_softmax(h, dim=1)
-   
+    def forward(self, x, edge_index, batch):
+        # Node embeddings
+        h1 = self.conv1(x, edge_index)
+        h2 = self.conv2(h1, edge_index)
+        h3 = self.conv3(h2, edge_index)
+        # Graph-level readout
+        h1 = global_add_pool(h1, batch)
+        h2 = global_add_pool(h2, batch)
+        h3 = global_add_pool(h3, batch)
+        # Concatenate graph embeddings
+        h = torch.cat((h1, h2, h3), dim=1)
+        # Classifier
+        h = self.lin1(h)
+        h = h.relu()
+        h = F.dropout(h, p=0.5, training=self.training)
+        h = self.lin2(h)
+        return F.log_softmax(h, dim=1)
+
+
 def train(model, loader):
-   criterion = torch.nn.CrossEntropyLoss()
-   optimizer = torch.optim.Adam(model.parameters(),lr=0.01)
-   epochs = 100
-   model.train()
-   for epoch in range(epochs+1):
-      total_loss = 0
-      acc = 0
-      val_loss = 0
-      val_acc = 0
-      # Train on batches
-   for data in loader:
-      optimizer.zero_grad()
-      out = model(data.x, data.edge_index, data.batch)
-      loss = criterion(out, data.y)
-      total_loss += loss / len(loader)
-      acc += accuracy(out.argmax(dim=1), data.y) /len(loader)
-      loss.backward()
-      optimizer.step()
-      # Validation
-      val_loss, val_acc = test(model, val_loader)
-      # Print metrics every 20 epochs
-      if(epoch % 20 == 0):
-         print(f'Epoch {epoch:>3} | Train Loss:{total_loss:.2f} | Train Acc: {acc*100:>5.2f}% | Val Loss: {val_loss:.2f} | Val Acc: {val_acc*100:.2f}%')
-      return model
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    epochs = 100
+    model.train()
+    for epoch in range(epochs + 1):
+        total_loss = 0
+        acc = 0
+        val_loss = 0
+        val_acc = 0
+        # Train on batches
+    for data in loader:
+        optimizer.zero_grad()
+        out = model(data.x, data.edge_index, data.batch)
+        loss = criterion(out, data.y)
+        total_loss += loss / len(loader)
+        acc += accuracy(out.argmax(dim=1), data.y) / len(loader)
+        loss.backward()
+        optimizer.step()
+        # Validation
+        val_loss, val_acc = test(model, val_loader)
+        # Print metrics every 20 epochs
+        if epoch % 20 == 0:
+            print(
+                f"Epoch {epoch:>3} | Train Loss:{total_loss:.2f} | Train Acc: {acc*100:>5.2f}% | Val Loss: {val_loss:.2f} | Val Acc: {val_acc*100:.2f}%"
+            )
+        return model
