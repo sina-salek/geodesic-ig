0cd3217b26a01c9b8157dcc152d8d15403be0a6d
sinasalek <salek.sina@gmail.com>
2025-01-01 08:54:18 +0100

    controlling which classifiers to use

--- experiments/voc/constants.py
+++ experiments/voc/constants.py
@@ -1,1 +1,2 @@
-VALID_BACKBONE_NAMES = ["resnet18", "convnext_base", "mobilenet_v3_small"]
+# VALID_BACKBONE_NAMES = ["resnet10t.c3_in1k", "resnet18", "convnext_base", "efficientnet_v2_l"]
+VALID_BACKBONE_NAMES = ["resnet10t.c3_in1k", "resnet18", "convnext_base"]
