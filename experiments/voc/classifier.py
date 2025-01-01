12060213fa6e59b151ee38e7a4e2bdd9e199cf61
sinasalek <salek.sina@gmail.com>
2025-01-01 08:53:15 +0100

    using more classifiers

--- experiments/voc/classifier.py
+++ experiments/voc/classifier.py
@@ -1,8 +1,8 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.models import resnet18, ResNet18_Weights, convnext_base, ConvNeXt_Base_Weights
-from torchvision.models import mobilenet_v3_small, MobileNet_V3_Small_Weights
-
+from torchvision.models import resnet18, ResNet18_Weights, convnext_base, ConvNeXt_Base_Weights, efficientnet_v2_l, EfficientNet_V2_L_Weights
+from torchvision.models import mobilenet_v3_small, MobileNet_V3_Small_Weights, shufflenet_v2_x0_5, ShuffleNet_V2_X0_5_Weights
+import timm
 from experiments.voc.constants import VALID_BACKBONE_NAMES
 
 class VocClassifier(nn.Module):
@@ -22,9 +22,36 @@ class VocClassifier(nn.Module):
             self.backbone.classifier[-1] = nn.Linear(num_features, 20)
         elif backbone_name == "mobilenet_v3_small":
             self.backbone = mobilenet_v3_small(weights=MobileNet_V3_Small_Weights.DEFAULT)
+            self.backbone = modify_mobilenet_activations(self.backbone)
+            num_features = self.backbone.classifier[-1].in_features
+            self.backbone.classifier[-1] = nn.Linear(num_features, 20)
+        elif backbone_name == "shufflenet_v2_x0_5":
+            self.backbone = shufflenet_v2_x0_5(weights=ShuffleNet_V2_X0_5_Weights.DEFAULT)
+            num_features = self.backbone.fc.in_features
+            self.backbone.fc = nn.Linear(num_features, 20)
+        elif backbone_name == "resnet10t.c3_in1k":  # Tiny ResNet10
+            self.backbone = timm.create_model('resnet10t.c3_in1k', pretrained=True)
+            num_features = self.backbone.fc.in_features
+            self.backbone.fc = nn.Linear(num_features, 20)
+        elif backbone_name == "efficientnet_v2_l":
+            self.backbone = efficientnet_v2_l(weights=EfficientNet_V2_L_Weights.DEFAULT)
             num_features = self.backbone.classifier[-1].in_features
             self.backbone.classifier[-1] = nn.Linear(num_features, 20)
     
     def forward(self, x):
         x = self.backbone(x)
         return F.log_softmax(x, dim=-1)
+    
+def modify_mobilenet_activations(model):
+    """Replace hardsigmoid/hardswish with differentiable alternatives in MobileNetV3"""
+    def _replace_activation(module):
+        for name, child in module.named_children():
+            if isinstance(child, nn.Hardswish):
+                setattr(module, name, nn.ReLU())
+            elif isinstance(child, nn.Hardsigmoid):
+                setattr(module, name, nn.Sigmoid())
+            else:
+                _replace_activation(child)
+    
+    _replace_activation(model)
+    return model
